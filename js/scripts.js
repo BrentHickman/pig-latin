118c6bbe2d127debb8f1fc80281f57d3aff99e9b
Brent <brenthickman@ymail.com>
2023-01-19 11:50:52 -0800

    test two passed
    Co-authored-by: Zookerman3 <szook7@gmail.com>
    
    Co-authored-by: Hernan Verar <hernanverar@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,19 @@
 //Business Logic
 
 
-
 function collectVowel(pigLatinInput){
-  let firstLetter = pigLatinInput.charAt(0);
   let vowelFlag = false;
+  const textArray = pigLatinInput.split(" ");
+  // let firstLetter = textArray.charAt(0);
+  const firstCharArray = textArray.map(function(element) {
+    return element.charAt(0);
+  });
+  console.log(firstCharArray);
+
   const vowels = ["a", "e", "i", "o", "u"];
   for (let i = 0; i <= vowels.length; i++){
-    if (firstLetter === vowels[i]){
+    if (firstCharArray === vowels[i]){
       vowelFlag = true;
-      break;
     }
     console.log("Looped!");
   }
